a402acfb142439fcd639ac96bbc731afe3f626ca
Jay A <athena@Athena.local>
2019-09-18 20:54:40 +0000

    Object.create

--- Object.create.js
+++ Object.create.js
@@ -5,6 +5,10 @@ const personPrototypes = {
     return `Hello there ${this.firstName} ${this.lastName}`;
 
   }
+
+  getsMarried: function(newLastName) {
+    this.lastName = newLastName; // updating the last name object
+  }
 }
 
 const mary = Object.create(personPrototypes);
@@ -12,3 +16,14 @@ const mary = Object.create(personPrototypes);
 mary.firstName = 'Mary';
 mary.lastName = 'Williams';
 mary.age = 30;
+
+mary.getsMarried('Thompson');
+
+console.log(mary.greeting());
+
+const brad = Object.create(personPrototypes, {
+
+  firstName: {value: 'Brad'},
+  lastName: {value: 'Smith'},
+  age: {value: 22}
+});
